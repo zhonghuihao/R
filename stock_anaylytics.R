aab141ab8734b5fbaf009acdf356e3e2166aadb1
zhonghuihao <zhonghuihao@users.noreply.github.com>
2015-07-27 13:46:00 +0800

    Update stock_anaylytics.R

--- stock_anaylytics.R
+++ stock_anaylytics.R
@@ -8,15 +8,14 @@ szzs <- getSymbols("SZZS",from = "2013-01-01",to = Sys.Date())
 setSymbolLookup(SWTX = list(name="002405.SZ", src="yahoo"))
 swtx <- getSymbols("SWTX",from = "2013-01-01",to = Sys.Date())
 
-
 ## 月指数和周指数
-SZZS.m <- to.monthly(SZZS)
-SZZS.w <- to.weekly(SZZS)
-SWTX.m <- to.monthly(SWTX)
-SWTX.w <- to.weekly(SWTX)
+SZZS.M <- to.monthly(SZZS)
+SZZS.W <- to.weekly(SZZS)
+SWTX.M <- to.monthly(SWTX)
+SWTX.W <- to.weekly(SWTX)
 
 require(TTR) 
-ser <- chartSeries(SZZS.w,theme = chartTheme("black",up.col="red",dn.col="green")) 
+ser <- chartSeries(SWTX.W,theme = chartTheme("white",up.col="red",dn.col="green")) 
 
 ## add moving averges to a chart 
 addSMA(5,on=1 ,with.col = Cl , overlay = T, col = "yellow")
@@ -29,10 +28,4 @@ addMACD(fast = 10, slow = 20, signal = 5,col = c("red", "green", "yellow", "red"
 
 ## add Bollinger bands
 addBBands()
-ser@colors$BBands.col <- "white"
-ser@colors$BBands.fille <-  "gray"
-reChart()
-
-
-
 
